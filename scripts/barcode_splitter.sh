b4621d6db0047247081ca70c0d9c3c363ee6fd35
MichaelSimons <msimons@microsoft.com>
2017-03-21 21:28:54 -0500

    Update barcode_splitter to be robust/user friendly

--- scripts/barcode_splitter.sh
+++ scripts/barcode_splitter.sh
@@ -1,36 +1,130 @@
 #!/usr/bin/env bash
 
-# Usage:  /tools/scripts/barcode_splitter.sh /usbdrive/CBBSeq/ks1.2000.fastq /usbdrive/bar-files 0mismatches_unmatchedinput 0 1
-
 # Stop script on NZEC
 set -e
 # Stop script if unbound variable found (use ${var:-} if intentional)
 set -u
 
-# better variable names
-# true options w/optional
-# fix ""
-# output stdout to file
+execute()
+{
+    echo "Executing: $@"
+    if [ $dryRun -eq 0 ]; then
+        "$@ |& tee -a $outputDir/output$suffix";
+    fi
+}
 
-fastq="$1"
-barcodes="$2"
-outdir="$3"
-nummismatches="$4"
-uselast="$5"
+say_err() {
+    printf "%b\n" "Error: $1" >&2
+}
 
-mkdir -p $outdir
+showHelp() {
+    echo "Usage: $0 [OPTIONS] FASTQ BARCODES"
+    echo
+    echo "Invokes the fastx_barcode_splitter tool on the specified fastqc file for the specified barcodes"
+    echo
+    echo "FASTQ                 The fastq input file"
+    echo "BARCODES              The directory that contains the files listing the barcodes to split on"
+    echo
+    echo "Options:"
+    echo "      --dryRun        Perform a dry run emitting the commands that would be invoked"
+    echo "  -h, --help          Show the help"
+    echo "  -m, --mismatches    The number of mismatches allowed"
+    echo "  -o, --output        The directory to write the output to"
+    echo "  -s, --suffix        The suffix of the output files"
+    echo "      --unmatched     Whether or not to run the subsequent splits on the unmatched results"
+}
 
-for barcode in `ls $barcodes/* | sort -r`
-do
-  basebarcodename=$(basename $barcode)
-  outname=$(echo $basebarcodename | cut -f 1 -d '.')
-  echo "Processing $barcode file..."
-  cmd="cat $fastq | /usr/local/bin/fastx_barcode_splitter.pl --bcfile $barcode --bol --mismatches $nummismatches --suffix \".txt\" --prefix $outdir/$outname |& tee -a $outdir/output.txt"
-  echo $cmd
-  eval $cmd
+barcodes=
+dryRun=0
+fastqInput=
+mismatches=0
+outputDir="barcode_splitter_results"
+splitterPath="/usr/local/bin/fastx_barcode_splitter.pl"
+suffix=".txt"
+useUnmatched=0
 
-  if [ $uselast -eq 1 ]
-  then
-    fastq="$outdir/$outname""unmatched.txt"
-  fi
+if [ $# -eq 0 ]; then
+    showHelp            
+    exit 1
+fi
+
+while [ $# -ne 0 ]; do
+    name=$1
+    case $name in  
+        --dryRun)
+            dryRun=1
+            ;;
+        -h|--help)
+            showHelp
+            exit 0
+            ;;
+        -m|--mismatches)
+            shift
+            mismatches=$1
+            ;;
+        -o|--output)
+            shift
+            outputDir="$1"
+            ;;
+        -s|--suffix)
+            shift
+            suffix="$1"
+            ;;
+        -u|--unmatched)
+            useUnmatched=1
+            ;;
+        -*)
+            say_err "Unknown option: $1"
+            showHelp            
+            exit 1
+            ;;
+        *)
+            if [ ! -z "$fastqInput" ]; then
+                if [ ! -z "$barcodes" ]; then
+                    say_err "Unknown argument: \`$name\`"
+                    exit 1
+                fi
+
+                barcodes="$1"
+            else
+                fastqInput="$1"
+            fi
+
+            ;;
+    esac
+
+    shift
+done
+
+if [ -z "$fastqInput" ]; then
+    say_err "fastqInput not specified"
+    showHelp            
+    exit 1
+fi
+
+if [ -z "$barcodes" ]; then
+    say_err "barcodes not specified"
+    showHelp            
+    exit 1
+fi
+
+if [ $dryRun -eq 0 ]; then
+    mkdir -p $outputDir
+fi
+
+for barcode in `ls $barcodes/* | sort -r`; do
+    barcodeBasename=$(basename $barcode)
+    outputFile=$(echo $barcodeBasename | cut -f 1 -d '.')
+    prefix="$outputDir/$outputFile"
+
+    echo "Processing $barcode file..."
+    cmd="cat $fastqInput | $splitterPath --bcfile $barcode --bol --mismatches $mismatches --suffix \"$suffix\" --prefix $prefix"
+    execute eval $cmd
+
+    unmatchedFile="${prefix}unmatched${suffix}"
+    if [ $useUnmatched -eq 1 ]; then
+        fastqInput="$unmatchedFile"
+    else
+        execute rm $unmatchedFile
+    fi
 done
